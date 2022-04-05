86581df75ead9739f2d5cd560167656c480d877c
jensneuse <jens.neuse@gmx.de>
2022-04-05 13:50:30 +0200

    Brew formula update for wunderctl version v0.74.3

--- Formula/wunderctl.rb
+++ Formula/wunderctl.rb
@@ -5,22 +5,22 @@
 class Wunderctl < Formula
   desc "wunderctl - the WunderGraph Command Line Interface"
   homepage "https://wundergraph.com/"
-  version "0.74.2"
+  version "0.74.3"
   license "MIT"
   depends_on :macos
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/wundergraph/wunderctl/releases/download/v0.74.2/wunderctl_0.74.2_Darwin_arm64.tar.gz", :using => CurlDownloadStrategy
-      sha256 "e5cc9312a07ba6eb6e310ddb02e209666b0f337578e35cc3c70dd5e5305c5965"
+      url "https://github.com/wundergraph/wunderctl/releases/download/v0.74.3/wunderctl_0.74.3_Darwin_arm64.tar.gz", :using => CurlDownloadStrategy
+      sha256 "8335eff49f1951bb3fc2e4a1c5f4680994840108f3c3442b16ca2c742195fbb7"
 
       def install
         bin.install "wunderctl"
       end
     end
     if Hardware::CPU.intel?
-      url "https://github.com/wundergraph/wunderctl/releases/download/v0.74.2/wunderctl_0.74.2_Darwin_x86_64.tar.gz", :using => CurlDownloadStrategy
-      sha256 "a78521926165f0142f814c619c655271905be78d1d30928c680cb23c975e8138"
+      url "https://github.com/wundergraph/wunderctl/releases/download/v0.74.3/wunderctl_0.74.3_Darwin_x86_64.tar.gz", :using => CurlDownloadStrategy
+      sha256 "b3f1a1239e17f9e16bcbc315234d94334074e71eca441bebdf1bb342a965da83"
 
       def install
         bin.install "wunderctl"
