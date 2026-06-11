703eda0f90cff7c64b6a59714e20062ef5d95888
Sam Gaus <sam@gaus.co.uk>
2026-06-11 12:22:57 +0100

    Pin wgc to node@24 LTS and add docs caveats

--- Formula/wgc.rb
+++ Formula/wgc.rb
@@ -8,11 +8,18 @@ class Wgc < Formula
   sha256 "9a2e1f5140b8601672cdd8a4b17a59e210c2a7180cc4cf3bb4895b5a8e69d184"
   license "Apache-2.0"
 
-  depends_on "node"
+  depends_on "node@24"
 
   def install
     system "npm", "install", *std_npm_args
     bin.install_symlink libexec.glob("bin/*")
+  end
+
+  def caveats
+    <<~EOS
+      Documentation is available at:
+        https://cosmo-docs.wundergraph.com/cli/intro
+    EOS
   end
 
   test do
